76904cfccb2805cc7c721f34675398b73bbb8f72
Developer0116 <helper0116@yahoo.com>
2019-06-27 12:51:01 -0400

    restaurant page fixed

--- src/pages/restaurant/index.js
+++ src/pages/restaurant/index.js
@@ -3,7 +3,6 @@ import renderHTML from 'react-render-html';
 import Topbar from '../../components/Topbar';
 import Footer from '../../components/Footer';
 import RestaurantForm from '../../components/RestaurantForm';
-import RestaurantImg1 from '../../assets/img/restaurant_img1.png';
 import RestaurantImg2 from '../../assets/img/restaurant_img2.png';
 
 import './style.scss';
@@ -17,12 +16,11 @@ export default class RestaurantPage extends React.Component {
 			<React.Fragment>
 				<div className={'main-panel'}>
 					<Topbar />
-					<div className={'main-panel-img1'}>
-						<img src={RestaurantImg1} />
-					</div>
+
 					<div className={'main-panel-img2'}>
 						<img src={RestaurantImg2} />
 					</div>
+					<div className={'panel-1-2'} />
 					<RestaurantForm />
 				</div>
 				<Footer />
